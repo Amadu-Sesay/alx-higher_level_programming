acec9d828d99bf9a7f57efa07fe94ec699501589
Alhaji Sesay <alhaji8765@gmail.com>
2023-01-03 15:53:35 -0800

    tash13

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,27 +1,49 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
-  * struct listint_s - singly linked list
-  * @n: integer
-  * @next: points to the next node
-  *
-  * Description: singly linked list node structure
-  * for Holberton project
-  */
-typedef struct listint_s
+ * insert_node - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @number: index of the list where the new node is
+ * added.
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *new;
+	listint_t *h;
+	listint_t *h_prev;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+	h = *head;
+	new = malloc(sizeof(listint_t));
 
-listint_t *insert_node(listint_t **head, int number);
+	if (new == NULL)
+		return (NULL);
 
-#endif /* LISTS_H */
+	while (h != NULL)
+	{
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
+	}
 
+	new->n = number;
+
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
+	}
+
+	return (new);
+}
