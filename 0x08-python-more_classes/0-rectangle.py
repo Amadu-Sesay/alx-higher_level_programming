e232fe6db5b40c90db11304060272224c644b46a
Alhaji Sesay <alhaji8765@gmail.com>
2023-02-01 06:32:38 -0800

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday January 25 2023
+@author: Alhaji Sesay
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
