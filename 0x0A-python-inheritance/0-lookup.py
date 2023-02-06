fe566fe6990908d80f977e714cd9a4258cd8ae8a
Alhaji Sesay <alhaji8765@gmail.com>
2023-02-06 13:20:33 -0800

    a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Monday Feb 06 20 : 49 2023
+@author: Alhaji Sesay
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
