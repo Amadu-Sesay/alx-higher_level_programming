873f676b50317b7b93b94867daae750910e19cc3
Alhaji Sesay <alhaji8765@gmail.com>
2023-04-11 15:48:26 -0700

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-const num = parseInt(arg);
-if (isNaN(num)) {
-  console.log("Missing number of occurrences");
+let i = 0;
+const argum = process.argv;
+if (argum.length === 2) {
+  console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < num; i++) {
-    console.log("C is fun");
+  while (i < argum[2]) {
+    console.log('C is fun');
+    i++;
   }
 }
